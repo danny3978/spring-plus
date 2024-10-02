6070eb0e8967f2bc1f5160f0a659d475f4047c05
dam <ekadl025@gmail.com>
2024-10-02 10:43:46 +0900

    필수 구현: level 1 - 2 (완)

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -18,23 +18,26 @@ public class User extends Timestamped {
     @Column(unique = true)
     private String email;
     private String password;
+    private String username;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email, String password, String username ,UserRole userRole) {
         this.email = email;
         this.password = password;
+        this.username = username;
         this.userRole = userRole;
     }
 
-    private User(Long id, String email, UserRole userRole) {
+    private User(Long id, String email, String username, UserRole userRole) {
         this.id = id;
         this.email = email;
+        this.username = username;
         this.userRole = userRole;
     }
 
     public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
+        return new User(authUser.getId(), authUser.getEmail(), authUser.getUsername(),authUser.getUserRole());
     }
 
     public void changePassword(String password) {
